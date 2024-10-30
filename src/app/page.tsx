76706729acce786b21d52b9d25ef0436b299df1b
ireh1214 <pir@driven.co.kr>
2024-10-30 14:53:08 +0900

    link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,12 @@ export default function Page() {
           </li>
           <li>
             <Link href="/trade">물물 교역 계산기</Link>
+          </li>{" "}
+          <li>
+            <Link href="/pocket">주머니 찾기(현재 안됨)</Link>
           </li>
           <li>
-            <Link href="/pocket">주머니 찾아보기</Link>
+            <Link href="/schedule">길드 일정표</Link>
           </li>
         </ul>
       </nav>
